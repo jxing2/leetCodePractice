27ae31d44546bdf0ded89c197e0d93be96a224e8
jxing <xingjinghao@persagy.com>
2022-05-11 01:33:41 +0800

    initiate Project

---
+++ javaCode/src/main/java/cc/ginx/p150_200/P200.java
@@ -0,0 +1,11 @@
+package cc.ginx.p150_200;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class P200 {
+    public int numIslands(char[][] grid) {
+        log.info("hello");
+        return 0;
+    }
+}
---
+++ javaCode/src/test/java/cc/ginx/p150_200/P200Test.java
@@ -0,0 +1,14 @@
+package cc.ginx.p150_200;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class P200Test {
+
+    @Test
+    void test200() {
+        System.out.println("hello");
+        log.info("aa");
+    }
+}
